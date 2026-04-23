67b0be9f35b16ac27c174055792c1cfced73bf8d
xylose666 <xuejiananyaya@gmail.com>
2026-04-23 23:39:07 +0800

    Add files via upload

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,11 +6,17 @@ import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import './styles/global.css'
 import App from './App.vue'
 import router from './router'
+import FadeIn from './directives/fadeIn'
+import Parallax from './directives/parallax'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus, { locale: zhCn })
+
+// global directives
+app.directive('fade-in', FadeIn)
+app.directive('parallax', Parallax)
 
 app.mount('#app')
